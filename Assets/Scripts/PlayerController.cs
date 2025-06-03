c42341e34302acf67e52cb96b65e6fc29db726b4
CaioFreitasRibeiro <136375048+CaioFreitasRibeiro@users.noreply.github.com>
2025-06-03 09:09:22 -0300

    Movimento do personagem
    Ragdoll e movimento estao programados, mas a animação nao funciona.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Animator animator;
     public bool canMove = true;
+    public RagdollActivator RagDoll;
+
+    private void Start()
+    {
+        RagDoll = GetComponent<RagdollActivator>();
+        //RagDoll.enabled = false;
+    }
 
     private void Update()
     {
@@ -14,13 +21,13 @@ public class PlayerController : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         Vector3 move = new Vector3(h, 0, v);
 
-        if (move.magnitude > 0.1f)
+        if (move.magnitude > 0.1f && RagDoll.enabled == true)
         {
             animator.SetBool("isWalking", true);
             transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
             transform.forward = move; // Rotaciona para direção do movimento
         }
-        else
+        else if (RagDoll.enabled == false)
         {
             animator.SetBool("isWalking", false);
         }
